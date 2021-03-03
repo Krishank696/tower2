aeb2d7c5b29e41ed7a5c1938e0dcd335320f4cb9
Krishank696 <73972335+Krishank696@users.noreply.github.com>
2021-03-03 16:47:25 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class base extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+    }
+  
+  };
---
+++ sketch.js
@@ -0,0 +1,40 @@
+  var ground, stand;
+  var b8,b9, b10, b11, b12, b13, b14, b15, b16;
+s
+
+
+  ground = new Ground(400, 400);
+  stand = new Ground(200, 200);
+  b8 = new block(330, 235, 30, 40);
+  b9 = new block(360, 235, 30, 40);
+  b10 = new block(390, 235, 30, 40);
+  b11= new block(420, 235, 30, 40);
+  b12= new block(450, 235, 30, 40);
+  b13= new block(360, 195, 30, 40);
+  b14= new block(390, 195, 30, 40);
+  b15 = new block(420, 195, 30, 40);
+  b16= new block(390, 155, 30, 40);
+ 
+
+  function draw(){
+   background(255);
+   
+   ground.display();
+   stand.display();
+   b8.display();
+   b9.display();
+   b10.display();
+   b11.display();
+   b12.display();
+   b13.display();
+   b14.display();
+   b15.display();
+   b16.display();
+   
+
+
+
+
+   displaySprites();
+
+  }
